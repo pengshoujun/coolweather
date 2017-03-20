014bf61130ab3fad01c8f8b9bd29cbd8e8815c2a
pengshoujun <925789322@qq.com   name=pengshoujun>
2017-03-21 04:26:00 +0800

    ”加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/province.java
@@ -0,0 +1,38 @@
+package com.coolweather.android.db;
+
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 925789322 on 2017/3/19.
+ */
+
+public class province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
